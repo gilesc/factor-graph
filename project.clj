2fd163cb4858f52206fb17dbde2ddff9be2aa615
Cory Giles <cory.b.giles@gmail.com>
2011-11-30 21:44:13 -0600

    move to factor graph representation

--- project.clj
+++ project.clj
@@ -1,5 +1,3 @@
-(defproject es.corygil/bayesnet "1.0.0-SNAPSHOT"
+(defproject es.corygil/factor-graph "1.0.0-SNAPSHOT"
   :description "Bayesian inference via belief propagation"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [incanter/incanter-core "1.3.0-SNAPSHOT"]
-                 [cc.artifice/loom "0.1.1"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]])
--- src/es/corygil/.#bayesnet.clj
+++
@@ -1,1 +0,0 @@
-gilesc@zangetsu.omrf.hsc.net.ou.edu.32109:1321394968
